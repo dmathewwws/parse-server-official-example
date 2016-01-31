15e3468b0edfef05858da04ad58d8d92e60f332e
Daniel Mathews <dannyl.mathews@gmail.com>
2016-01-31 11:19:07 -0800

    added env keys

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:argon
+FROM node:4.2.6-wheezy
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
@@ -14,9 +14,9 @@ ENV APP_ID myAppId
 ENV MASTER_KEY mySecretMasterKey
 ENV FILE_KEY optionalFileKey
 ENV DATABASE_URI mongodb://dmathewwws:or4dmdmuR6jekc@ds051645.mongolab.com:51645/parse-server-example
-#ENV JAVASCRIPT_KEY javascriptKey
-#ENV REST_API_KEY restAPIKey
-#ENV CLIENT_KEY clientKey
+ENV JAVASCRIPT_KEY javascriptKey
+ENV REST_API_KEY restAPIKey
+ENV CLIENT_KEY clientKey
 
 EXPOSE 1337
 
--- index.js
+++ index.js
@@ -12,9 +12,13 @@ if (!process.env.DATABASE_URI) {
 var api = new ParseServer({
   databaseURI: process.env.DATABASE_URI || 'mongodb://localhost:27017/dev',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: 'myAppId',
-  masterKey: 'myMasterKey'
+  appId: process.env.APP_ID,
+  masterKey: process.env.MASTER_KEY,
+  javascriptKey: process.env.JAVASCRIPT_KEY,
+  clientKey: process.env.CLIENT_KEY,
+  restAPIKey: process.env.REST_API_KEY
 });
+
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
